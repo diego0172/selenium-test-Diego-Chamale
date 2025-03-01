8ba3d8f142760221399bb21dbfba6b432e3ceb33
eaguerral <edgarlira142001@gmail.com>
2025-02-28 20:58:25 -0600

    Funcionamiento de visita a paginas web y interaccion en ellas

--- src/test/java/com/etc/NavegarTest.java
+++ src/test/java/com/etc/NavegarTest.java
@@ -1,36 +1,29 @@
 package com.etc;
 
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class NavegarTest {
+    WebDriver driver;
 
-    public class navegarTest {
-        private WebDriver driver;
-
-        @BeforeClass
-        public void setUp() {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-        }
-
-        @Test
-        public void testNavegacion() {
-            driver.get("https://www.google.com");
-            assert driver.getTitle().contains("Google");
-        }
-
-        @AfterClass
-        public void tearDown() {
-            if (driver != null) {
-                driver.quit();
-            }
-        }
+    @BeforeTest
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
     }
 
+    @Test
+    public void abrirGoogle() {
+        driver.get("https://www.google.com");
+        System.out.println("Página abierta: " + driver.getTitle());
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+    }
 }
